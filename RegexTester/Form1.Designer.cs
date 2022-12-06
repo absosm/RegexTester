d8d9b2bac7bb7036d729b5fcb72c8946378a5a5c
absosm <absosm@gmail.com>
2022-12-06 23:05:28 +0100

    Add project files.

---
+++ RegexTester/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace RegexTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_match = new System.Windows.Forms.Button();
+            this.rtb_information = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lb_count = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lb_groups = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(659, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "([^\\r\\n\\\\]|\\\\.|\\\\\\n)+";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_match
+            // 
+            this.btn_match.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_match.Location = new System.Drawing.Point(677, 10);
+            this.btn_match.Name = "btn_match";
+            this.btn_match.Size = new System.Drawing.Size(75, 28);
+            this.btn_match.TabIndex = 1;
+            this.btn_match.Text = "Match";
+            this.btn_match.UseVisualStyleBackColor = true;
+            this.btn_match.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtb_information
+            // 
+            this.rtb_information.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb_information.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtb_information.Location = new System.Drawing.Point(12, 44);
+            this.rtb_information.Name = "rtb_information";
+            this.rtb_information.Size = new System.Drawing.Size(740, 172);
+            this.rtb_information.TabIndex = 2;
+            this.rtb_information.Text = "AT+CUSD=1,\"2A994C3602\",15<CR><CR><LF>OK<CR><LF>\n<CR><LF>+CUSD: 2<CR><LF>\nAT<CR><C" +
+    "R><LF>OK<CR><LF>";
+            this.rtb_information.TextChanged += new System.EventHandler(this.rtb_information_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.lb_count,
+            this.toolStripStatusLabel2,
+            this.lb_groups});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 382);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(761, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(55, 17);
+            this.toolStripStatusLabel1.Text = "Matches:";
+            // 
+            // lb_count
+            // 
+            this.lb_count.Name = "lb_count";
+            this.lb_count.Size = new System.Drawing.Size(13, 17);
+            this.lb_count.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(45, 17);
+            this.toolStripStatusLabel2.Text = "Groups";
+            // 
+            // lb_groups
+            // 
+            this.lb_groups.Name = "lb_groups";
+            this.lb_groups.Size = new System.Drawing.Size(13, 17);
+            this.lb_groups.Text = "0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 222);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(362, 147);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(380, 222);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(369, 147);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 404);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.rtb_information);
+            this.Controls.Add(this.btn_match);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RegexTester 0.1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_match;
+        private System.Windows.Forms.RichTextBox rtb_information;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lb_count;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel lb_groups;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ RegexTester/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegexTester
+{
+    public partial class Form1 : Form
+    {
+        private MatchCollection matches;
+        public Form1()
+        {
+            InitializeComponent();
+
+            rtb_information.Text = "cmd1\r\ncmd2\r\ncmd3\r\ncmd4";
+            button1_Click(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rtb_information.SelectionStart = 0;
+            rtb_information.SelectionLength = rtb_information.TextLength;
+            rtb_information.SelectionBackColor = Color.White;
+            rtb_information.SelectionColor = Color.Black;
+
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                lb_count.Text = "0";
+                return;
+            }
+
+            try
+            {
+                lb_count.Text = Regex.Matches(rtb_information.Text, textBox1.Text).Count.ToString();
+                matches = Regex.Matches(rtb_information.Text, textBox1.Text);
+                listBox1.Items.Clear();
+                for(int i=0; i<matches.Count; i++)
+                    listBox1.Items.Add($"match ({i}) \"{matches[i].Value}\"");
+
+                foreach (Match match in Regex.Matches(rtb_information.Text, textBox1.Text))
+                {
+                    if (match.Success)
+                    {
+                        rtb_information.SelectionStart = match.Index;
+                        rtb_information.SelectionLength = match.Length;
+                        rtb_information.SelectionColor = Color.White;
+                        rtb_information.SelectionBackColor = Color.Red;
+                    }
+                }
+            }
+            catch (Exception) { lb_count.Text = "0"; }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            button1_Click(null, null);
+        }
+
+        private void rtb_information_TextChanged(object sender, EventArgs e)
+        {
+            int i = rtb_information.SelectionStart;
+            textBox1_TextChanged(null, null);
+            rtb_information.SelectionStart = i;
+            rtb_information.SelectionLength = 0;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lb_groups.Text = matches[listBox1.SelectedIndex].Groups.Count.ToString();
+            listBox2.Items.Clear();
+            for (int i=0; i < matches[listBox1.SelectedIndex].Groups.Count; i++)
+                listBox2.Items.Add($"group ({i}) \"" +
+                    $"{matches[listBox1.SelectedIndex].Groups[i].Value}\"");
+        }
+    }
+}
